d7e2ea59d5a08f92601079b133535813ed384675
prasst <prassaiyan@gmail.com>
2019-06-18 10:04:03 +0700

    Test simple command

--- bot-run.php
+++ bot-run.php
@@ -4,16 +4,22 @@ include __DIR__.'/vendor/autoload.php';
 include __DIR__.'/config/config.php';
 
 $discord = new \Discord\Discord([
-    'token' => _TOKEN,
+  'token' => _TOKEN,
 ]);
 
 $discord->on('ready', function ($discord) {
-    echo "Bot is ready.", PHP_EOL;
-  
-    // Listen for events here
-    $discord->on('message', function ($message) {
-        echo "Recieved a message from {$message->author->username}: {$message->content}", PHP_EOL;
-    });
+  echo "Bot is ready.", PHP_EOL;
+
+  // Listen for events here
+  $discord->on('message', function ($message) {
+      echo "Recieved a message from {$message->author->username}: {$message->content}", PHP_EOL;
+  });
 });
+
+$discord_command = new DiscordCommandClient([
+  'token' => _TOKEN,
+]);
+
+$discord->registerCommand('!umimi', 'http://shizuka.765.moe/fdhCa.gif');
 
 $discord->run();
